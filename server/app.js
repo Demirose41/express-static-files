07bfe71b3f75c530ad4b7f6960c2a3bd3cadf7a3
demirose41 <demirose41@gmail.com>
2024-12-01 22:08:03 -0800

    completed parts 2 and 3

--- server/app.js
+++ server/app.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const app = express();
-// Your code here
-app.use('/css', express.static("assets/css"))
-app.use('/images', express.static("assets/images"))
-app.use('/scripts', express.static("assets/scripts"))
+// Part 1
+// app.use('/css', express.static("assets/css"))
+// app.use('/images', express.static("assets/images"))
+// app.use('/scripts', express.static("assets/scripts"))
+
+// Part 2
+// app.use("/scripts", express.static("assets/scripts"))
+
+// Part 3
+app.use("/stylesheets", express.static("assets/css"))
 
 const port = 5000;
 app.listen(port, () => console.log('Server is listening on port', port));
